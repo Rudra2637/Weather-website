f9faa0fce896278dec0e44577989d968e9ff5a91
Rudra Pratap Singh <145354880+Rudra2637@users.noreply.github.com>
2025-02-10 23:12:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const input = document.querySelector("#search");
+const button = document.querySelector("#submit");
+
+
+
+button.addEventListener("click",()=>{
+    const value = input.value.trim();
+    if(!value){
+        alert("Please enter a city name");
+        return;
+    }
+    const url =`https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=87b8502249e686620b7affc0f5e214bd&units=metric`;
+   
+    fetch(url).then((response)=>{
+        if(response.ok){
+            return response.json();
+        }
+        throw new Error("No city found");
+    }).then((data)=>{
+        document.querySelector("h2").innerHTML = data.name;
+        document.querySelector("#temp").innerHTML=data.main.temp + "°C";
+        document.querySelector("#desc").innerHTML=data.weather[0].description;
+        document.querySelector("img").src = "http://openweathermap.org/img/w/"+data.weather[0].icon+".png";
+        document.querySelector("img").classList.add("icon");
+        console.log(document.querySelector("img"));
+    }).catch((error)=>{
+        console.log(error);
+    })
+})
+
+
